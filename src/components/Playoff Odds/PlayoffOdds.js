3601a38c58992f1414216d02a078eb8dc22a7df4
stevenpfaff <pfaffy1426@gmail.com>
2024-12-11 09:23:04 -0600

    stat update

--- src/components/Playoff Odds/PlayoffOdds.js
+++ src/components/Playoff Odds/PlayoffOdds.js
@@ -66,7 +66,7 @@ class PlayoffOdds extends Component {
     return (
       <div className="table-container">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <p>Updated as of 12/10/2024</p>
+        <p>Updated as of 12/11/2024</p>
         <Table striped bordered hover>
           <thead>
             <tr>
