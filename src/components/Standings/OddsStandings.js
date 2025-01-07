45297b0df141596fdebff1b46ad44c4d2d84eb49
stevenpfaff <pfaffy1426@gmail.com>
2025-01-07 11:18:45 -0600

    stat update

--- src/components/Standings/OddsStandings.js
+++ src/components/Standings/OddsStandings.js
@@ -58,7 +58,7 @@ class OddsStandings extends Component {
     return (
       <div>
         <h1 style={{ marginTop: '2%' }}>NHL Playoff Odds</h1>
-        <p>Last Updated 1/5/2025</p>
+        <p>Last Updated 1/7/2025</p>
       <div className="divisionOdds-container">
         <div className="division-column">
           <Table striped bordered hover>
