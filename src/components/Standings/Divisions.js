1c9554e4bd28ed078afb5876c2cf17ce1e78961b
stevenpfaff <pfaffy1426@gmail.com>
2025-04-02 10:10:37 -0500

    stat update

--- src/components/Standings/Divisions.js
+++ src/components/Standings/Divisions.js
@@ -57,7 +57,7 @@ class Divisions extends Component {
           />
           NHL Projected Standings
         </h1>
-        <p>Updated as of 4/1/2025</p>
+        <p>Updated as of 4/2/2025</p>
         <div className="divisions-container">
           <div className="division-column">
             <Table striped bordered hover>
