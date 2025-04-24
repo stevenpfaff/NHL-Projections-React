b1ebdf3adb7057e843d2a9fe02352908a8459551
stevenpfaff <pfaffy1426@gmail.com>
2025-04-24 08:26:33 -0500

    navbar update

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,17 +11,17 @@ const NavBar = () => {
         <Nav className="mx-auto custom-nav-links">
         <img src={logo} alt="..." height="50" width="50"></img>
           <Nav.Link href="/">Home</Nav.Link>
-          <NavDropdown title="Projections">
+          <NavDropdown title="NHL Playoffs">
             <NavDropdown.Item href="/PlayoffOdds">Playoff Odds</NavDropdown.Item>
-            <NavDropdown.Item href="/finalresults">Final Results</NavDropdown.Item>
             {/* <NavDropdown.Item href="/OddsDivisional">Playoff Odds Standings View</NavDropdown.Item> */}
             {/* <NavDropdown.Item href="/SimulatedPoints">Simulated Points</NavDropdown.Item> */}
           </NavDropdown>
           <NavDropdown title="Preseason Projections">
-          <NavDropdown.Item href="/PreseasonOdds">Preseason Playoff Odds</NavDropdown.Item>
+            <NavDropdown.Item href="/PreseasonOdds">Preseason Playoff Odds</NavDropdown.Item>
             <NavDropdown.Item href="/PreseasonStandings">Preseason Projected Standings</NavDropdown.Item>
             <NavDropdown.Item href="/Picks">Preseason Picks</NavDropdown.Item>
             <NavDropdown.Item href="/Percentiles">Preseason Percentile Outcomes</NavDropdown.Item>
+            <NavDropdown.Item href="/finalresults">Final Results</NavDropdown.Item>
           </NavDropdown>
           <Nav.Link href="/PowerRankings">Power Rankings</Nav.Link>
           <Nav.Link href="/Timeline">Stanley Cup Odds Timeline</Nav.Link>
