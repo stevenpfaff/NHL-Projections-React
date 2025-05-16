e767ca347303ae57e9aefee15a893273244483c2
stevenpfaff <pfaffy1426@gmail.com>
2025-05-16 08:26:49 -0500

    stat update

--- src/components/Playoff Odds/PlayoffOdds.js
+++ src/components/Playoff Odds/PlayoffOdds.js
@@ -75,7 +75,7 @@ render() {
           />
           NHL Playoff Odds
         </h1>
-        <p>Updated as of 5/15/2025</p>
+        <p>Updated as of 5/16/2025</p>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Table striped bordered hover responsive size="sm">
         <thead>
