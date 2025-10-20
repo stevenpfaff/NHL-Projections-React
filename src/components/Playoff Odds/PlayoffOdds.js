6c592f6aa9695caea545a3555edb2387db2352b7
stevenpfaff <pfaffy1426@gmail.com>
2025-10-20 06:45:50 -0500

    10/20 update

--- src/components/Playoff Odds/PlayoffOdds.js
+++ src/components/Playoff Odds/PlayoffOdds.js
@@ -85,7 +85,7 @@ const PlayoffOdds = () => {
         />
         NHL Playoff Odds
       </h1>
-      <p>Updated as of 10/19/2025</p>
+      <p>Updated as of 10/20/2025</p>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <Table
