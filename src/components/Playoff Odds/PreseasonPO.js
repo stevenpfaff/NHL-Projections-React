33af11e12e22198a02e3b6ecea14c3f93732ec57
stevenpfaff <pfaffy1426@gmail.com>
2026-09-18 09:26:25 -0500

    minor tweak

--- src/components/Playoff Odds/PreseasonPO.js
+++ src/components/Playoff Odds/PreseasonPO.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import Papa from 'papaparse';
 import './PlayoffOdds.css';
+import { useParams } from 'react-router-dom';
 
 const PreseasonOdds = () => {
   const { year } = useParams();
